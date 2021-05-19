38e5cf914774ef973308c39f3574eb8f5dffdbdf
Sophie Townsend <se.townsend31@gmail.com>
2021-05-19 15:17:45 +1000

    Updates include setting up the first row to win if red

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-
-
 //Declare variables 
-var cells = document.querySelectorAll('.cell');
+//DOM Elements
+var cells = document.querySelectorAll('.cell-div');
+var playerTurnSection = document.querySelector('.players-turn-section')
+
+//General Elements
 // 1 = player 1, 2 = player 2
-var playerID = 2;
+var playerID = 1;
+
 
 //Functions 
 function handleClick(e){
-    debugger
     var cell = e.target;
 
     if (playerID === 1){
@@ -16,16 +18,31 @@ function handleClick(e){
         cell.style.backgroundColor = 'blue'
     }
 
+    if(checkforWinner()){
+        console.log("There is a winner");
+    }
+    
     changePlayer();
 }
 
 function changePlayer(){
     if (playerID === 1){
+        playerTurnSection.textContent = `Player 2's turn`;
        return playerID = 2;
     } else {
+        playerTurnSection.textContent = `Player 1's turn`;
         return playerID = 1;
     }
 
+}
+
+function checkforWinner(){
+    for (var i=0; i<3; i++){
+        if (cells[i].style.backgroundColor != 'red'){
+            return false;
+        }
+    }
+    return true;
 }
 
 //Events
